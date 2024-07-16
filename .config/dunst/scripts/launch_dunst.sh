e4a0cc8670457c0f4c77c708fc9de5ec879622a7
DanielSanzz7 <sanzzdaniel7@gmail.com>
2024-07-15 22:49:59 -0600

    Updates

--- .config/dunst/scripts/launch_dunst.sh
+++ .config/dunst/scripts/launch_dunst.sh
@@ -8,9 +8,9 @@
 source $HOME/.config/rofi/scripts/globalcontrol.sh
 
 if [[ "$current_theme" -eq 1 ]]; then
-    sleep 0.1
+    usleep 10000
     dunst -conf "~/.cache/wal/dunstrc-dark" &
 else
-    sleep 0.1
+    usleep 10000
     dunst -conf "~/.cache/wal/dunstrc-light" &
 fi
