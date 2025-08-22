faf016eac313bb5531f7114ab4ae363be5546632
DanielSanzz7 <sanzzdaniel7@gmail.com>
2025-08-22 14:25:46 -0600

    Updates

--- .config/hypr/scripts/batterynotify.sh
+++ .config/hypr/scripts/batterynotify.sh
@@ -4,6 +4,9 @@
 # | Battery System Notify |
 #  -----------------------
 
+export DISPLAY=:1
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
 # Current icon theme
 icontheme=$(grep -o "gtk-icon-theme-name.*" ~/.config/gtk-3.0/settings.ini | cut -d "=" -f 2)
 
@@ -14,11 +17,11 @@ BATVAL=$(cat /sys/class/power_supply/${BAT}/capacity)
 BATICON=$(geticons "battery" -s 16 -c 1 -t "$icontheme" --no-fallbacks | head -n 1)
 
 if [[ "$BATSTATUS" == "Discharging" ]];then
-    if [ "$BATVAL" -lt 40 ];then
-        notify-send -i $BATICON "Battery Level" "Battery less than 40% 󰁾 - Please connect your laptop"
-    elif [ "$BATVAL" -lt 25 ];then
-        notify-send -u low -i $BATICON "Battery Level" "Battery less than 25% 󰁺 - Please connect your laptop"
-    elif [ "$BATVAL" -lt 10 ];then
-        notify-send -u critical -i $BATICON "Battery Level" "Battery less than 10% 󰁺 - Please connect your laptop"
+    if [ "$BATVAL" -eq 45 ];then
+        notify-send -i $BATICON "Battery Level" "Battery equal to 45% 󰁾 - Please connect your laptop"
+    elif [ "$BATVAL" -eq 30 ];then
+        notify-send -u low -i $BATICON "Battery Level" "Battery equal to 30% 󰁺 - Please connect your laptop"
+    elif [ "$BATVAL" -lt 20 ];then
+        notify-send -u critical -i $BATICON "Battery Level" "Battery less than 20% 󰁺 - Please connect your laptop"
     fi
 fi
